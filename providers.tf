f8e5ef8a69c94030021068ca010895332f2cfc72
Naimul Hassan Noor <49410883+naimulNoor@users.noreply.github.com>
2022-08-23 20:30:56 +0600

    Update providers.tf
    init

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   features {}
     use_msi   = true
-    subscription_id = "863fda5d-6174-4448-8f7b-f8b0f9008431"
-    client_id       = "656fbb4a-59b1-421a-a449-baba545af22c"
-    tenant_id       = "12025f5d-3ae2-41ea-8b1c-7b41d3f025c4"
+    #subscription_id = "863fda5d-6174-4448-8f7b-f8b0f9008431"
+    #client_id       = "656fbb4a-59b1-421a-a449-baba545af22c"
+    #tenant_id       = "12025f5d-3ae2-41ea-8b1c-7b41d3f025c4"
 }
 
 
